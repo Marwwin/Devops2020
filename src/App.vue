a7c1929553d234c83101c2778d45d5f38a0e498a
nuggex <nuggex@gmail.com>
2020-10-05 13:45:04 +0300

    Initial startpoint
    bra skit

---
+++ src/App.vue
@@ -0,0 +1,64 @@
+<template>
+  <div id="app">
+    <Header />
+    <!-- Skicka in en fråga till barnkomponenten -->
+    <Quiz @clicked="checkaSvar" :currentQuestion="questions[current]" />
+    
+
+  </div>
+</template>
+
+<script>
+import Header from './components/Header.vue'
+import Quiz from './components/Quiz.vue'
+
+
+export default {
+  name: 'App',
+  components: {
+    Header,
+    Quiz,
+
+  },
+  methods:{
+    checkaSvar(answered){
+      alert("föräldern tog emot emmisson, " + answered);
+      if(answered == this.questions[this.current].correct){
+        this.current++;
+      }
+    },
+  },
+  data : function(){
+    return {
+      // Skapar en array med våra frågor
+
+      questions: [
+        {
+          number: 1,
+          question: "Hur går det",
+          answers: ["Bra", "Dåligt", "Semi"],
+          correct: 2
+        },
+        {
+          number: 2,
+          question: "Är 5 myror fler än fyra elefanter",
+          answers: ["Ja", "Nej", "Semi"],
+          correct: 0
+        }
+      ],
+      current: 0
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/Quiz.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="card">
+    <h5>Fråga {{currentQuestion.number}}</h5>
+    <p>{{currentQuestion.question}}</p>
+    <div class="answers-container mt-5">
+      <p @click="checkaSvar(index)" v-bind:number="index" v-for="(answer, index) in currentQuestion.answers" :key="index" class="btn btn-outline-primary btn-lg m-3">
+        <a >{{answer}}</a>
+      </p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Quiz",
+  data: function () {
+    return {
+      answers: [
+        { number: 1, text: "yaas" },
+        { number: 2, text: "qeeeen" },
+      ],
+    };
+  },
+  props:{
+    currentQuestion: Object
+  },
+  methods:{
+
+    checkaSvar(ans){
+      this.$emit("clicked", ans);
+    },
+    check(inp){
+      if( (Number) (inp.target.attributes[0].nodeValue) == this.currentQuestion.correct){
+        alert("Correct");
+      }else{
+        alert("no");
+      }
+      
+    },
+    checkChild(inp){
+      console.log(inp);
+       if( (Number) (inp.parentElement.attributes[0].nodeValue) == this.currentQuestion.correct){
+        alert("Correct");
+      }else{
+        alert("no");
+      }
+    }
+  }
+};
+</script>
