f3f6831234a44c0ee4bb7f1bb615dcd57ff63135
nuggex <nuggex@gmail.com>
2020-10-05 15:50:03 +0300

    CORS

--- src/server.js
+++ src/server.js
@@ -1,6 +1,7 @@
 const http = require('http');
 const app = require('./app');
-
+const cors = require('cors');
+app.use(cors);
 const port = process.env.port || 8080;
 
 const server = http.createServer(app)
