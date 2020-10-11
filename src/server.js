7d946a39ec44bd09fb7c65791d221ce4672c7e58
nuggex <nuggex@gmail.com>
2020-10-11 20:43:59 +0300

    logo

--- src/server.js
+++ src/server.js
@@ -40,6 +40,7 @@ app.use((req, res, next) => {
 });
 
 app.use((error, req, res, next) => {
+    next;
     res.status(error.status || 500).json({
         status: error.status,
         error: error.message
