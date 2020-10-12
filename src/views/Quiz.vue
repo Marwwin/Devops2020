428a23b2b2dd181d364f85b838e32142e8b0d5f1
saarishe <70575886+saarishe@users.noreply.github.com>
2020-10-12 15:39:35 +0300

    Quiz buttons

--- src/views/Quiz.vue
+++ src/views/Quiz.vue
@@ -1,27 +1,26 @@
 <template>
   <div class="quiz">
-    <!-- WHY TF IS THE TEXT NOT IN THE MIDDLE-->
-<div class="outer">
-<div class="box"><h1>Easy</h1></div>
-<div class="box" style="margin : 15px"><h1>Medium</h1></div>
-<div class="box"> <h1>Cry</h1><br>
-</div>
-</div>
 
     <!-- How does one get question from that DB-->
-    <h1>Quiz</h1>
+    <h1>Quiz</h1><br>
     <div class="question">
-    <h1>Question 1</h1>
+    <h2>Question 1</h2>
     <p>What's the name of Helsinki?</p>
     <input type="text" id="quiz-answer" />
-    <div class="question"></div>
-
-  </div>
+    </div><br>
+    <div class="question">
+    <h2>Question 2</h2>
+    <p>Who said "Livet är ett möte?"</p>
+    <input type="text" id="quiz-answer" />
+    </div><br>
+    <button id="answer-btn">Submit</button>
+    <button id="cancel-btn" onclick="document.location='../'">Cancel</button>
+  
   </div>
 </template>
 
 <style scoped>
-.outer{
+/*.outer{
         padding-top: 4%;
         list-style-type: none;
         text-align: center;
@@ -38,12 +37,34 @@
     border: darkslateblue;
     border-style: solid;
     border-radius: 4%;
-    
-transition: all 1s ease;
+    transition: all 1s ease;
     }
 
     .box:hover{
        padding: 8.5%;
        background-color: rgba(71, 61, 139, 0.301);
+    }*/
+
+    #answer-btn{
+      padding: 0 20px;
+     width: 15vw;
+    height: 6vh;
+      background-color: #333333;
+      border-radius: 10px;
+      color:  rgb(223, 185, 90);
+      margin: 5px ;
+      border: none;
     }
+
+    #cancel-btn{
+      padding: 0 20px;
+      margin: 5px ;
+     width: 15vw;
+    height: 6vh;
+      background-color: #70707088;
+      border-radius: 10px;
+      color:  black;
+      border: none;
+    }
+
 </style>
