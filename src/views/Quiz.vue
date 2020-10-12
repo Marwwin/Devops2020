1da478cd588c9342883c4047f375ae9451b1f22d
saarishe <70575886+saarishe@users.noreply.github.com>
2020-10-12 16:19:30 +0300

    changes in Quiz text

--- src/views/Quiz.vue
+++ src/views/Quiz.vue
@@ -1,15 +1,15 @@
 <template>
   <div class="quiz">
-
+    <br>
     <!-- How does one get question from that DB-->
-    <h1>Quiz</h1><br>
+    <h2>START QUIZ</h2>
     <div class="question">
-    <h2>Question 1</h2>
+    <h3>Question 1</h3>
     <p>What's the name of Helsinki?</p>
     <input type="text" id="quiz-answer" />
     </div><br>
     <div class="question">
-    <h2>Question 2</h2>
+    <h3>Question 2</h3>
     <p>Who said "Livet är ett möte?"</p>
     <input type="text" id="quiz-answer" />
     </div><br>
@@ -20,31 +20,6 @@
 </template>
 
 <style scoped>
-/*.outer{
-        padding-top: 4%;
-        list-style-type: none;
-        text-align: center;
-        margin: 0;
-        padding: 0;
-    }
-    .box{
-    display: inline-flex;
-    height: 10vh;
-    width: 10vw;
-    text-align: center;
-    padding: 8%;
-    background-color: white;
-    border: darkslateblue;
-    border-style: solid;
-    border-radius: 4%;
-    transition: all 1s ease;
-    }
-
-    .box:hover{
-       padding: 8.5%;
-       background-color: rgba(71, 61, 139, 0.301);
-    }*/
-
     #answer-btn{
       padding: 0 20px;
      width: 15vw;
