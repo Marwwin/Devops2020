2f997622efae4627a44e5ce9ed6805264ec419d8
Marwwin <53864900+Marwwin@users.noreply.github.com>
2020-10-19 12:18:31 +0300

    Update App.vue
    breaking for testing

--- src/App.vue
+++ src/App.vue
@@ -7,7 +7,7 @@
 </template>
 
 <script>
-import Header from "./components/Header.vue";
+import Headera from "./components/Header.vue";
 import Footer from "./components/Footer.vue";
 
 export default {
